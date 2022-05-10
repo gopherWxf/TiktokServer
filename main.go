f5e7260d0fe665277ee9a2c1a541a8380d8ef771
“wxf” <68725032@qq.com>
2022-05-11 00:44:46 +0800

    连接数据库

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"TiktokServer/opdb"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	//从配置文件中读取数据库的配置信息并连接数据库
+	if dbErr := opdb.InitMySqlConn(); dbErr != nil {
+		panic(dbErr)
+	}
+	defer opdb.DB.Close()
+	//初始化表结构
+	opdb.InitModel()
 	r := gin.Default()
 	//后续看情况改成https,暂时不急
 	initRouter(r)
--- opdb/initializeDB.go
+++ opdb/initializeDB.go
@@ -1,1 +1,61 @@
 package opdb
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"log"
+)
+import "gopkg.in/ini.v1"
+
+type DBConfig struct {
+	Host     string `json:"host"`
+	Port     int    `json:"port"`
+	User     string `json:"user"`
+	Pwd      string `json:"pwd"`
+	Database string `json:"database"`
+}
+
+//解析ini文件并反射到结构体中
+func parserConfig(dbCfg *DBConfig) {
+	cfg, err := ini.Load("./opdb/config.ini")
+	if err != nil {
+		log.Panicf("Fail to read file: %v\n", err)
+	}
+	dbCfg.Host = cfg.Section("DB").Key("host").String()
+	dbCfg.Port, _ = cfg.Section("DB").Key("port").Int()
+	dbCfg.User = cfg.Section("DB").Key("user").String()
+	dbCfg.Pwd = cfg.Section("DB").Key("pwd").String()
+	dbCfg.Database = cfg.Section("DB").Key("database").String()
+}
+
+//读取配置文件内容
+func LoadDBConfig() *DBConfig {
+	dbCfg := &DBConfig{}
+	//解析ini文件并反射到结构体中
+	parserConfig(dbCfg)
+	return dbCfg
+}
+
+var DB *gorm.DB
+
+//从配置文件中读取数据库的配置信息并连接数据库
+func InitMySqlConn() (err error) {
+	//读取配置文件内容
+	dbCfg := LoadDBConfig()
+	//拼凑连接数据库的语句
+	connStr := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbCfg.User, dbCfg.Pwd, dbCfg.Host, dbCfg.Port, dbCfg.Database)
+	log.Println(connStr)
+	//连接数据库
+	DB, err = gorm.Open("mysql", connStr)
+	if err != nil {
+		return err
+	}
+	//检测数据库是否活跃
+	return DB.DB().Ping()
+}
+func InitModel() {
+	//表--->如果没有则自动创建，如果有则添加该结构体新的字段
+	//TODO 明天去设计表
+	//DB.AutoMigrate(&User{}) //如果表不存在,通过User这个结构体建表
+}
