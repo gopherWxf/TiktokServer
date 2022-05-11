f8683a1ac0b087103bdc1c681120ef256a06eacb
“wxf” <68725032@qq.com>
2022-05-11 11:57:59 +0800

    实现注册

--- controller/user.go
+++ controller/user.go
@@ -1,11 +1,58 @@
 package controller
 
 import (
+	"TiktokServer/dfst"
+	"TiktokServer/opdb"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
+//新用户注册时提供用户名，密码，昵称即可，用户名需要保证唯一。创建成功后返回用户 id 和权限token
 func Register(c *gin.Context) {
-
+	username := c.Query("username")
+	password := c.Query("password")
+	if len(username) > 32 || len(password) > 32 || username == "" || password == "" {
+		c.JSON(http.StatusOK, dfst.RegisterResponse{
+			Status: dfst.Status{
+				StatusCode: -1,
+				StatusMsg:  "用户名或密码过长",
+			},
+		})
+		return
+	}
+	//将用户信息插入数据库中
+	rr := dfst.RegisterRequest{Username: username, Password: password}
+	userInfo, err := opdb.Register(rr)
+	//用户已存在或插入错误
+	if err != nil {
+		c.JSON(http.StatusOK, dfst.RegisterResponse{
+			Status: dfst.Status{
+				StatusCode: -1,
+				StatusMsg:  err.Error(),
+			},
+		})
+		return
+	}
+	token, err := generateToken(c, username)
+	if err != nil {
+		c.JSON(http.StatusOK, dfst.RegisterResponse{
+			Status: dfst.Status{
+				StatusCode: -1,
+				StatusMsg:  err.Error(),
+			},
+		})
+		return
+	}
+	//注册成功
+	c.JSON(http.StatusOK, dfst.RegisterResponse{
+		Status: dfst.Status{
+			StatusCode: 0,
+			StatusMsg:  "注册成功",
+		}, IdAndToken: dfst.IdAndToken{
+			UserId: userInfo.Id,
+			Token:  token,
+		},
+	})
 }
 
 func Login(c *gin.Context) {
--- opdb/opUser.go
+++ opdb/opUser.go
@@ -1,1 +1,48 @@
 package opdb
+
+import (
+	"TiktokServer/dfst"
+	"errors"
+)
+
+func Register(rr dfst.RegisterRequest) (_ UserInfo, _ error) {
+	//查看该用户是否在数据库中
+	if UserIsExists(rr.Username) {
+		return UserInfo{}, errors.New("用户已经存在,请直接登陆")
+	}
+	userInfo := UserInfo{
+		Name:          rr.Username,
+		Password:      rr.Password,
+		FollowCount:   0,
+		FollowerCount: 0,
+	}
+	//将用户信息插入数据库中
+	err := userInfo.Insert()
+	if err != nil {
+		return UserInfo{}, err
+	}
+	userInfo.GetInfo()
+	return userInfo, nil
+}
+
+//查看该用户是否在数据库中
+func UserIsExists(name string) bool {
+	result := false
+	// 指定库
+	var user UserInfo
+	dbResult := DB.Where("name = ?", name).Find(&user)
+	if dbResult.Error != nil {
+		result = false
+	} else {
+		result = true
+	}
+	return result
+}
+
+//将用户信息插入数据库中
+func (user *UserInfo) Insert() error {
+	return DB.Model(&UserInfo{}).Create(&user).Error
+}
+func (user *UserInfo) GetInfo() {
+	DB.Find(user, "name=? && password=?", user.Name, user.Password)
+}
